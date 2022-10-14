09a7c7f56fbfc5f31292c3690c2bac1edd17ceca
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-10-14 22:14:49 +0800

    updated

--- C-Exercises/1Fpractice.c
+++ C-Exercises/1Fpractice.c
@@ -10,12 +10,33 @@ int numberOne(){
     char str[100];
     sprintf(str, "%d", num);
 
-    char lastDigit = str[sizeof()];
-    printf("%c", lastDigit);
+
+    printf("%c \n", str[strlen(str)-1]);
 }
 
-int numberTwo(){
+// int numberTwo(){
+//     /*Write a program that will get four numbers from the sue and display the following output.*/
+//     float numOne, numTwo, numThree, numFour;
+//     printf("Enter the first number: ");
+//     scanf("%d", &numberOne);
+//     printf("Enter the second number: ");
+//     scanf("%d", &numberTwo);
+//     printf("Enter the third number: ");
+//     scanf("%d", &numberThree);
+//     printf("Enter the fourth number: ");
+//     scanf("%d", &numFour);
 
+//     printf("******** average is %f ********", (numberOne + numberTwo + numThree + numFour) / 4);
+
+//     printf("First number: %d -- ", numberOne);
+// }
+
+int numberThree(){
+    /*Write a C program that calculate a student's average score for a course 
+    with 4 quizzes, 2 midterms and a final score. The quizzes are 
+    weighted 30%, the midterms 40% and the final 30%*/
+
+  
 }
 
 int main(){
@@ -28,6 +49,9 @@ int main(){
     }
 
     else if(userInput == 2){
-        numberTwo();
+        // numberTwo();
+    }
+    else if(userInput == 3){
+        numberThree();
     }
 }
