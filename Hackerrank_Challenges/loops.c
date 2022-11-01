b4dcb775469d96efe4ee983d3400103e94c1e228
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-01 17:54:44 +0800

    New Challenge

--- Hackerrank_Challenges/loops.c
+++ Hackerrank_Challenges/loops.c
@@ -8,6 +8,16 @@ int main() {
     int n;
     scanf("%d", &n);
     //Complete the code to calculate the sum of the five digits on n.
-    printf("%d", n[0]);
+    int sum;
+    char num[5];
+    itoa(n, num, 1);
+    for(int i = 0; i<=sizeof(num)/sizeof(num[0]); ++i){
+        int x;
+        char *y = num[i];
+        sscanf(y, "%d", &x);
+        sum += x;
+    }
+
+    printf("%d", sum);
     return 0;
 }
