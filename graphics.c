a6ee71c19e6c4a1a62c17ac1d8d5be48ecf2b680
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-09 23:55:07 +0800

    New and Update

--- graphics.c
+++ graphics.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 
-void main(){
-    int gdriver = DETECT, gmode;
-    int x1 = 200, y1 = 200;
-    int x2 = 300, y2 = 300;
-    clrscr();
+void main()
+{
 
-    initgraph(&gdriver, &gmode, "c:\\turboc3\\bgi");
-    line(x1, y1, x2, y2);
-    getch();
-    closegraph();
+    char name[5];
+
+do{
+    printf("Rule no.1: Don't enter any invalid input.\n");
+    printf("Yes or No[Yes/No]: ");
+    scanf("%s", name);
+
+    strcmp();
+    if(name == "Yes" || name == "YES" || name == "yes"){
+            printf("You're in a loop.");
+    }
+    else if(name == "No" || name == "NO" || name == "yes"){
+           printf("You're in a loop."); 
+    }
+    else{
+        printf("I TOLD YOU, DON'T ENTER ANY INVALID OUTPUT!");
+    }
+}
+while(name == "Yes" || name == "YES" || name == "yes" && name == "No" || name == "NO" || name == "no");        
 }
