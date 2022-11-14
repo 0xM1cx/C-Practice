89301fb2f8d4c74aed3bdffaa46c2fd67a3c7095
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-14 20:44:15 +0800

    updated

--- IT_134_Quiz/loops.c
+++ IT_134_Quiz/loops.c
@@ -2,7 +2,30 @@
 
 int main(){
 
-    int ctrl1;
+    int ctrl1, ctrl2, lines;
+    printf("How many lines? ");
+    scanf("%d", &lines);
+    ctrl2 = lines;
+    while (ctrl2 >= 1)
+    {
+        ctrl1 = 1;
+        while (ctrl1 <= ctrl2)
+        {
+            printf(" ");
+            ctrl1++;
+        }
+        ctrl1 = lines;
+        while (ctrl1 >= ctrl2)
+        {
+            printf("*");
+            ctrl1--;
+        }
+        printf("\n");
+        ctrl2--;
+        
+        
+    }
+    
 
     return 0;
 }
