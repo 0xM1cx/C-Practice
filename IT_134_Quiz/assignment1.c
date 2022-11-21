0baf925184c337078ee3fb6c9c13fc05785281b4
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-21 18:59:02 +0800

    Update

--- IT_134_Quiz/assignment1.c
+++ IT_134_Quiz/assignment1.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 void main(){
-    
+    /*Create a program that accepts an integer. The Program displays a pattern. If the input is an
+    even number, the next number is half of the previous number. If the number is odd, the next number
+    is multiplied by 3 and added by 1... The next number is displayed until the next number is 1.*/
+
+    int userInt;
+    printf("INTEGER: ");
+    scanf("%d", &userInt);
+
+
 }
