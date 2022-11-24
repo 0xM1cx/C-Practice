a214ad0f1ffd6e5362d7bc88c08ddd57899e4767
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-24 18:31:05 +0800

    Update

--- IT_134_Quiz/CharlesProject.c
+++ IT_134_Quiz/CharlesProject.c
@@ -15,33 +15,34 @@ void findWinner(int playerOneSum, int playerTwoSum){
     }
 }
 
-// int getCards(){
+/*Lucky 9
+Ung array is range from{ 'A', 2-10)
+Tapos {Hearts, Spades, Clover, Diamond}
 
-//     // Step 4
-//     srand(time(0)); // Para mag babago yung random number everytime na matatawag itong getCards()
-    
-//     int randNums[2];
-//     int randNumsSymbols[2];
-//     int TotalrandNums[4];
 
-    
-//     // Getting the symbols for the card
-//     for(int i = 0; i <= 1; i++){
-//         TotalrandNums[i] = rand() % 3;
-//     }
+1. Mag aask si program ng name ng dalawag player, tpaos un yung magiging name ni Player 1 and Player 2. (Ex. Player Josh and Player Earl)
 
-//     // Getting the car number
-//     for(int b = 2; b<=3; b++){
-//         TotalrandNums[b] = rand() % 9;
-//     }
-    
-//     return TotalrandNums;
-// }
+2. Next, may lalabas na "Card is shuffling........." (delay for 10s
+
+3. "Card is giving to both players" 
+
+4. Player 1 card "9 of hearts and 7 of spades" tapos kay player 2 din ganon
+
+5. (5s bago lumabas to) Player Josh, you want another card? If yes, magbibigay ng card pero naka "* of ****" muna, if No proceed na kay player 2.  Pagdating kay player 2,ganon din mag aask din mun,a if Yes print "Your card is ****" ganyan. 
+
+7. Lalabas ung total cards, kasama ung isang extra na card na inask ni program 
+
+8. After non, lalabas na ung result ganto
+"Player 1 total card: 1 of hearts, 4 of spades and 3 of  diamonds with a total acore of 8" same kay player 2 ganyan din.
+
+9. If more than 9 ung score, minus 10 dapat, so kapag 10 ung total, magiging 0 ung score, kapag 11 naman, magiging 1 ung score 
+
+10. (3s bago lumabas to) Player 1 is the winner.*/
 
 
 void main(){
     // Initializing variables with their specified values
-    char num[10] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10'};
+    char num[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10'};
     char shapes[4][10] = {"Hearts", "Spades", "Clubs", "Diamonds"};
 
     // Ask players for their names
@@ -61,21 +62,21 @@ void main(){
     printf("\nCard is giving to both players");
     sleep(5);
 
-
+    srand(time(0));
     goto getPlayerOneCards;
     goto getPlayerTwoCards;
     
     // Generating Cards for Player 1
-    getPlayerOneCards:
-        srand(time(0)); // Para mag babago yung random number everytime na matatawag itong getCards()
-        
         int randNums[2];
         int randNumsSymbols[2];
+    getPlayerOneCards:
+         // Para mag babago yung random number everytime na matatawag itong getCards()
+        
 
         
         // Getting the symbols for the card
         for(int i = 0; i <= 1; i++){
-            randNumsSymbols[i] = rand() % 3;
+            randNumsSymbols[i] = rand() % 4;
         }
 
         // Getting the car number
@@ -85,16 +86,16 @@ void main(){
 
 
     // Generating Cards for Player 2
-    getPlayerTwoCards:
-        srand(time(0)); // Para mag babago yung random number everytime na matatawag itong getCards()
-        
         int _randNums[2];
         int _randNumsSymbols[2];
+    getPlayerTwoCards:
+     // Para mag babago yung random number everytime na matatawag itong getCards()
+        
 
         
         // Getting the symbols for the card
         for(int i = 0; i <= 1; i++){
-            _randNumsSymbols[i] = rand() % 3;
+            _randNumsSymbols[i] = rand() % 4;
         }
 
         // Getting the car number
@@ -102,16 +103,29 @@ void main(){
             _randNums[b] = rand() % 9;
         }
     
- 
-    int playerOneSum = num[randNums[0]] + num[randNums[1]];
-    int playerTwoSum = num[_randNums[0]] + num[_randNums[1]];
+   
+    int playerOneSum = (num[randNums[0]] - '0') + (num[randNums[1]] - '0');
+    int playerTwoSum = (num[_randNums[0]] - '0') + (num[_randNums[1]] - '0');
+
+    printf("\n%d", playerOneSum);
+    printf("\n%d\n", playerTwoSum);
 
     
     // Displaying Cards to the USER
-    system("cls");
-    printf("Player %s: %d of %d and %d of %d\n", playerOne, num[randNums[0]], shapes[randNumsSymbols[0]], num[randNums[1]], shapes[randNumsSymbols[1]]);
-    printf("Player %s: %d of %d and %d of %d", playerTwo, num[_randNums[0]], shapes[_randNumsSymbols[0]], num[_randNums[1]], shapes[_randNumsSymbols[1]]);
+    // The the purpose of the conditions is to see if it is 1 because 1 in lucky 9 is A, so it will replace  1 with A.
+    if(randNums[0] == '1'){
+        randNums[0] = 'A';
+        printf("\nPlayer %s: %c of %s and %c of %s\n", playerTwo, num[_randNums[0]], shapes[_randNumsSymbols[0]], num[_randNums[1]], shapes[_randNumsSymbols[1]]);
+        printf("Player %s: %c of %s and %c of %s\n", playerOne, num[randNums[0]], shapes[randNumsSymbols[0]], num[randNums[1]], shapes[randNumsSymbols[1]]);
+    }else if(randNums[1] == '1'){
+        randNums[1] = 'A'; 
+        printf("\nPlayer %s: %c of %s and %c of %s\n", playerTwo, num[_randNums[0]], shapes[_randNumsSymbols[0]], num[_randNums[1]], shapes[_randNumsSymbols[1]]);
+        printf("Player %s: %c of %s and %c of %s\n", playerOne, num[randNums[0]], shapes[randNumsSymbols[0]], num[randNums[1]], shapes[randNumsSymbols[1]]);
+    }else{
+        printf("\nPlayer %s: %c of %s and %c of %s\n", playerTwo, num[_randNums[0]], shapes[_randNumsSymbols[0]], num[_randNums[1]], shapes[_randNumsSymbols[1]]);
+        printf("Player %s: %c of %s and %c of %s\n", playerOne, num[randNums[0]], shapes[randNumsSymbols[0]], num[randNums[1]], shapes[randNumsSymbols[1]]);
+    }
 
-
+    findWinner(playerOneSum, playerTwoSum);
 
 } 
