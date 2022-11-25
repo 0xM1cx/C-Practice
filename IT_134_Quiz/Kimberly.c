c53bd7304003295b8b39d5d5057f262d5679c41c
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-25 22:39:40 +0800

    Update

--- IT_134_Quiz/Kimberly.c
+++ IT_134_Quiz/Kimberly.c
@@ -15,6 +15,10 @@ void BookOne(){
 
 void main(){
 
+    // Initializing gameplay system variables
+    int playerLife = 10;
+    
+
     // Ask the player for his/her name
     char playerName[20];
     printf("PLAYER NAME: ");
