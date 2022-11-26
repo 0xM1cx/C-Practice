834f0223f7d73c365b17b33047a717912774e340
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-26 20:24:23 +0800

    New Assignment

---
+++ IT_134_Quiz/assignment4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main(){
+
+    /*Create a program that displyas a 10 x 10 random integers from 1 to
+    50. The program counts the number of odd integers as well as the 
+    number of even integers. The sum of both even numbers and odd numbers
+    are also displayed.
+    
+    SAMPLE OUTPUT:
+    1 2 3 5 2 6 2 4 7 2
+    4 5 2 6 4 8 5 6 6 3
+    1 2 4 3 6 8 5 9 5 8
+    4 3 6 8 2 9 4 8 4 0
+    
+    Number of Odd:
+    Number of Even:
+    Sum of Odd:
+    Sum of Even: */
+
+
+
+}
