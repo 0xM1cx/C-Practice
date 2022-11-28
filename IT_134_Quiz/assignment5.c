587ac0c2465cf62c0c9c386df5115b581b98e2dd
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-11-28 15:47:56 +0800

    New Assignment

---
+++ IT_134_Quiz/assignment5.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void main(){
+
+    /*Create a program that displyas a 10 x 10 random integers from 1 to
+    50. The program counts the number of odd integers as well as the 
+    number of even integers. The sum of both even numbers and odd numbers
+    are also displayed. This time use an array
+    
+    SAMPLE OUTPUT:
+    1 2 3 5 2 6 2 4 7 2
+    4 5 2 6 4 8 5 6 6 3
+    1 2 4 3 6 8 5 9 5 8
+    4 3 6 8 2 9 4 8 4 0
+    
+    Number of Odd:
+    Number of Even:
+    Sum of Odd:
+    Sum of Even: */
+    
+    srand(time(0));
+    int randNum;
+    int numberOfOdd = 0;
+    int numberOfEven = 0;
+    int sumOfEven = 0, sumOfOdd = 0;
+    int arrRandNum[100];
+    int counter = 0;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int b = 1; b <= 10; b++)
+        {
+            randNum = rand() % 50 + 1;
+            arrRandNum[counter] = randNum;
+            counter++;
+            if (randNum % 2 == 0)
+            {
+                numberOfEven += 1;
+                sumOfEven += randNum;
+            }else{
+                
+                numberOfOdd += 1;
+                sumOfOdd += randNum;
+            }
+
+            printf("%4d", randNum);
+            
+        } 
+        printf("\n");
+    }
+
+    printf("\nNumber of Even: %d", numberOfEven);
+    printf("\nNumber of Odd: %d", numberOfOdd );
+    printf("\nSum of Odd: %d", sumOfOdd);
+    printf("\nSum of Even: %d", sumOfEven);
+
+    printf("\n");
+    for(int i = 1; i <= 100; i++){
+        printf("%4d", arrRandNum[i-1]);
+        if(i % 10 == 0){
+            printf("\n");
+        }
+    }
+
+    
+    
+
+
+}
