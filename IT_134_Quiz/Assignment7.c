7762b36f6c0b4317e497e656f85f36ee1fb20e30
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-04 14:08:58 +0800

    UPDATE

--- IT_134_Quiz/Assignment7.c
+++ IT_134_Quiz/Assignment7.c
@@ -8,30 +8,32 @@
 
 
 void main(){
-    srand(time(0)); 
-    
-    // Make a 7x7 2 dimensional array
-    int array[size][size]; // Initialize the 7x7 array
-    for (int i = 0; i <size; i++)
-    {
-        for (int b = 0; b < size; b++)
-        {
-            // Generating a random number for every element in the array.
-            array[i][b] = rand() % 50 + 1; 
-        }     
-    }
 
     
+    // I created an array of integers from 1 to 49 
+    int array[size][size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 
+    47, 48, 49}; // Initialize the 7x7 array
+
+    // This loop below is used to display the loop as specified by the instructions(girls)
     for (int i = 0; i <size; i++)
     {
-        for (int b = 0; b < size; b++)
+        for (int b = 0; b < size; b++) // notice that the size is 7. So this loop will run from array[0] to array[6]
         {
-            printf("%4d", array[i][b]);
+            printf("%4d", array[b][i]); // the i represent the row and the b represents the column
         }
-        printf("\n");
-           
+        printf("\n");   
     }
 
-    printf("%d", array[1][1]);
+    /*When you run the code above the output will look like this
+    1 2 3 4 5 6 7
+    8 9 10 11 12 13 14
+    15 16 17 18 19 20 21
+    22 23 24 25 26 27 28...*/
+
+    // Now diba sinabi ko na ying i above kay ang row tapos yung b is ang column. 
+    // So base sa output natin i piprint natin, as an example, yung row 3 column 0 dapat yung output ay 22.
+    // NOTE: We start counting from zero
+    printf("\n\nThe number in row 3, column 0 is: %d", array[3][0]);
     
 }
