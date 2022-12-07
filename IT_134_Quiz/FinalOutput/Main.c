016a1ce2cff1bfc2c8ef1d8f2e89efa1068ad6c4
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-07 12:39:45 +0800

    Update

--- IT_134_Quiz/FinalOutput/Main.c
+++ IT_134_Quiz/FinalOutput/Main.c
@@ -2,17 +2,19 @@
 #include <time.h>
 #include <stdlib.h>
 
-    int rollCounter = 1;
-
-
+int rollCounter = 1;
 
 void play(){
     srand(time(0));
 
-    int dieOne = rand() % 6 + 1;
+    int dieOne = rand() % 6 + 1; // Invokes a random number for 
     int dieTwo = rand() % 6 + 1;
     int sum = dieOne + dieTwo;
 
+
+
+    // Set input for the user to be the next random number
+    // isWinner will be the flag 
     int isWinner = 0;
     for(;isWinner != 1; rollCounter++){
         if(rollCounter == 1){
@@ -29,16 +31,17 @@ void play(){
 
 void main(){
     char UserInput;
-    printf("WELCOME PLAYER");
-    printf("[P]lay\n[E]xit");
-    scanf("%c", &UserInput);
+    printf("WELCOME PLAYER\n\n");
+    printf("[P]lay\n[E]xit\n");
+    scanf(" %c", &UserInput);
 
     if(UserInput == 'P' || UserInput == 'p'){
         play();
     }
     else if(UserInput == 'E' || UserInput == 'e'){
         exit(0);
-    }else{
+    }
+    else{
         printf("Invalid Input, it must be either P or E");
         sleep(3);
         main();
