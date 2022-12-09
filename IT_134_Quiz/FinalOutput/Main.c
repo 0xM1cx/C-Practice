1e48873a8c4f61c2828207d02aa3a52b4b370c56
shawn michael sudaria <shawnlovecode14@gmail.com>
2022-12-09 14:04:20 +0800

    update

--- IT_134_Quiz/FinalOutput/Main.c
+++ IT_134_Quiz/FinalOutput/Main.c
@@ -8,11 +8,11 @@
 
 // Declaring the functions to be used
 int getRanNums(); 
-void succedingRolls(int userBet, int valToWin), play(int userBet), bet(), playAgain(), displayInfo(), rules();
+void succedingRolls(int userBet, int valToWin), play(int userBet), bet(), playAgain(), displayInfo(), rules(), loadingAnimation(), displayBanner();
 
 // Declaring and initializing the global variables to be used
 int currentCapital = 1000;
-int RollsArr[size][size];
+int RollsArr[size][3];
 int currentRoll = 0;
 
 
@@ -22,43 +22,21 @@ void main(){
     
     system("cls");
 
-    if (currentCapital == 0)
-    {
-        char usrInput;
-        printf("No More Money.\n\n\nThank You For Playing");
-        printf("\n\nPress Any Key to Continue");
-        scanf(" %c", &usrInput);
-        exit(0);
-    }
+    
     
     srand(time(0));
     char UserInput; // This is used to get the user input
 
-    printf("\033[0;37m");
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",95,95,95,220, 223, 223, 223, 220,220,220,220,220,220,220,223,223,223,220,95,95,95);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 196,196,196, 219, 178,178, 178,178,178,178,178,178,178,178,178,178,178,219,196,196,196);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 196,196,196, 196,219,178,178,219,178,178,178,178,178,219,178,178,219,196,196,196);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 196,220,220,196,196,219,178,178,178,223,219,223,178,178,178,219,196,196,220,220,196);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 219,178,178,219,196,223,220,178,178,178,178,178,178,178,220,223,196,219,178,178,219);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,219);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,178,178, 203,196,203, 201,187,203,196,201,187,201,187,201,203,187,201,187,178,219);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,178,178,  186,186,186,  204,196, 186,196,186,196,186,186,186,186,186,204,196,178,219);
-    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,178,178, 200,202,188,  200,188, 200,188, 200,188, 200,188,202,196,202, 200,188,178,219);
-    printf("\t\t\t %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n\n",219,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,219);
+    displayBanner();
     printf("\033[0;32m");
 
-    currentRoll = 0; // this is used for the number of the row used in the 2d array
-    // printf("\033[0;37m");
-    // printf("=========================\n\n");
-    // printf("\033[0;34m");
-    // printf("WELCOME TO ROLL OR DIE\n\n");
-    // printf("\033[0;37m");
-    // printf("=========================\n\n\n");
+    
+
 
     
     displayInfo();
 
-    printf("Current Capital: %d\n\n\n", currentCapital); // Displaying the current capital to the user
+    
     printf("[P]lay\n[E]xit\n\nINPUT: ");
     scanf(" %c", &UserInput);
 
@@ -76,10 +54,42 @@ void main(){
 }
 
 
+void displayBanner(){
+    printf("\033[0;37m");
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",95,95,95,220, 223, 223, 223, 220,220,220,220,220,220,220,223,223,223,220,95,95,95);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 196,196,196, 219, 178,178, 178,178,178,178,178,178,178,178,178,178,178,219,196,196,196);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 196,196,196, 196,219,178,178,219,178,178,178,178,178,219,178,178,219,196,196,196);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 196,220,220,196,196,219,178,178,178,223,219,223,178,178,178,219,196,196,220,220,196);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 219,178,178,219,196,223,220,178,178,178,178,178,178,178,220,223,196,219,178,178,219);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,223,219);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,178,178, 203,196,203, 201,187,203,196,201,187,201,187,201,203,187,201,187,178,219);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,178,178,  186,186,186,  204,196, 186,196,186,196,186,186,186,186,186,204,196,178,219);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",219,178,178, 200,202,188,  200,188, 200,188, 200,188, 200,188,202,196,202, 200,188,178,219);
+    printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n\n",219,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,220,219);
+}
+
+void loadingAnimation(){
+    for (int i = 0; i<=0; i++) {
+        sleep(1);
+        printf("\b\\");
+        
+        sleep(1);
+        printf("\b|");
+    
+        sleep(1);
+        printf("\b/");
+    
+        sleep(1);
+        printf("\b-");
+        sleep(1);
+    }
+}
+
+
 void playAgain(){
     char userInput;
     
-    printf("Do you want to play again? Y or N\nINPUT: ");
+    printf("\n\n\nDo you want to play again? Y or N\nINPUT: ");
     scanf(" %c", &userInput);
 
     if(userInput == 'Y' || userInput == 'y'){
@@ -120,70 +130,50 @@ int getRanNums(){
 void succedingRolls(int userBet, int valToWin){
 
     srand(time(0));
-    int valueToWin = valToWin;
+    int _valueToWin = valToWin;
     int isDone = 1;
     while(isDone){
 
         int randNum = getRanNums();
+        printf("\n\nRolling Dice and Getting sum  ");
+        loadingAnimation();
+        printf("\n");
+        
         printf("Roll %d\n\t", currentRoll+1);
         printf("D1: %d\n\t", RollsArr[currentRoll][0]);
         printf("D2: %d\n\t", RollsArr[currentRoll][1]);
         printf("T: %d\n\n", RollsArr[currentRoll][2]);
 
-        if(randNum == valueToWin){
-            printf("Winner!!\n\n");
+        if(randNum == _valueToWin){
+            printf("\n\n\nWinner!!\n\n");
             currentCapital += userBet;
+            printf("Current Capital: %d\n\n\n", currentCapital);
+            loadingAnimation();
             playAgain();
             isDone = 0;
         }else if (randNum == 7)
         {
-            printf("You Lost!!\n\n");
+            printf("\n\n\nYou Lost!!\n\n");
             currentCapital -= userBet;
+            printf("Current Capital: %d\n\n\n", currentCapital);
+            loadingAnimation();
             playAgain();
             isDone = 0;
+        }else{
+            _valueToWin = randNum;
+            currentRoll++;
         }
-        currentRoll++;
-
-        printf("\n\nRolling Dice and Getting sum  ");
-        for (int i = 0; i<=0; i++) {
-            sleep(1);
-            printf("\b\\");
-            
-            sleep(1);
-            printf("\b|");
-        
-            sleep(1);
-            printf("\b/");
-        
-            sleep(1);
-            printf("\b--");
-            sleep(1);
-    }
-        printf("\n");
     }
 }
 
 
 void play(int userBet){
-
-    
     int randNum = getRanNums();
 
+    currentRoll = 0; // this is used for the number of the row used in the 2d array
+
     printf("\n\nRolling Dice and Getting sum  ");
-    for (int i = 0; i<=0; i++) {
-        sleep(1);
-        printf("\b\\");
-        
-        sleep(1);
-        printf("\b|");
-       
-        sleep(1);
-        printf("\b/");
-       
-        sleep(1);
-        printf("\b--");
-        sleep(1);
-    }
+    loadingAnimation();
     
 
     printf("\n\nRoll 1\n\t");
@@ -195,31 +185,20 @@ void play(int userBet){
     
     if (randNum == 7)
     {
-        printf("We have a winner\n\n");
+        printf("\n\n\nWe have a winner\n\n");
         currentCapital += userBet;
+        printf("Current Capital: %d\n\n\n", currentCapital);
+        loadingAnimation();
         playAgain();
     }
     else if(randNum == 11 || randNum == 2){
-        printf("You have lost\n\n");
+        printf("\n\n\nYou have lost\n\n");
         currentCapital -= userBet;
+        printf("Current Capital: %d\n\n\n", currentCapital);
+        loadingAnimation();
         playAgain();
+        
     }else{
-        printf("\n\nRolling Dice and Getting sum  ");
-        for (int i = 0; i<=0; i++) {
-            sleep(1);
-            printf("\b\\");
-            
-            sleep(1);
-            printf("\b|");
-        
-            sleep(1);
-            printf("\b/");
-        
-            sleep(1);
-            printf("\b--");
-            sleep(1);
-    }
-        printf("\n");
         succedingRolls(userBet, randNum);
     }           
 }
@@ -227,14 +206,29 @@ void play(int userBet){
 void bet(){
     int userBet;
 
-    printf("\nYOUR BET: ");
-    scanf("%d", &userBet);
-    if(userBet <= 0 || userBet >currentCapital){
-        printf("Invalid Bet");
-        sleep(3);
-        bet();
+    system("cls");
+
+    displayBanner();
+    printf("\033[0;32m");
+
+    if (currentCapital == 0)
+    {
+        char usrInput;
+        printf("Insufficient Balance.\n\n\nThank You For Playing");
+        printf("\n\nPress Any Key to Continue");
+        scanf(" %c", &usrInput);
+        exit(0);
     }
 
+    printf("Current Capital: %d\n\n\n", currentCapital); // Displaying the current capital to the user
+    printf("\nYOUR BET: ");
+    scanf("%d", &userBet);
+
+    if(userBet <= 0 || userBet > currentCapital){
+        printf("Invalid Bet");
+        loadingAnimation();
+        bet();
+    }
     play(userBet);
 }
 
