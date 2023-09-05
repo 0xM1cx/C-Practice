82edc375077c7dbbe5b7dd713024b93796039368
Shun <shawnlovecode14@gmail.com>
2023-09-05 07:45:28 -0400

    New Folder for DSA Learning

---
+++ C-DSA/one.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int arr[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
+	for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+		cout << arr[i] << endl;
+	}
+	return 0;
+}
