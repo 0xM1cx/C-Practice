1565f5cdc1479c94972262fcd01336e01147216c
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-09-09 00:25:57 +0800

    DSA Sample Program for Assignment

---
+++ C-DSA/AssignmentOne.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N, NUM1, SMALL, COUNT;
+
+    // Step 1: Input the number of integers to compare an N
+    cout << "Enter the number of integers to compare N: ";
+    cin >> N;
+
+    // Step 2: Input the first integer an NUM1
+    cout << "Enter the first integer NUM1: ";
+    cin >> NUM1;
+
+    // Step 3: Initialize SMALL and COUNT
+    SMALL = NUM1;
+    COUNT = 2;
+
+    // Step 4: Loop to compare integers
+    while (COUNT <= N) {
+        int nextNum;
+        // Input the next integer
+        cout << "Enter integer #" << COUNT << ": ";
+        cin >> nextNum;
+
+        // Step 5: Compare NUM1 with SMALL and update SMALL if needed
+        if (nextNum < SMALL) {
+            SMALL = nextNum;
+        }
+
+        // Step 6: Increment COUNT
+        COUNT++;
+
+        // Step 7: Return to step 5
+    }
+
+    // Step 8: Ig display la an value han SMALL. Optional la ine na variable.
+    cout << "The smallest integer is: " << SMALL << std::endl;
+
+    return 0;
+}
