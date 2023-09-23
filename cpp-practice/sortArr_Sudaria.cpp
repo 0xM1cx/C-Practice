2439e6b0f9bbb4c2b82bfe572175742b2429cfcd
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-09-23 22:16:12 +0800

    updated

--- cpp-practice/sortArr_Sudaria.cpp
+++ cpp-practice/sortArr_Sudaria.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-
+#include <windows.h>
 using namespace std;
 
 void loading() {
     for (int i = 0; i < 5; i++) {
-        /* code */
+        Sleep(90);
+        cout << "\b\\";
+        Sleep(90);
+        cout << "\b|";
+        Sleep(90);
+        cout << "\b/";
+        Sleep(90);
+        cout << "\b-";
     }
+    cout << "\b";
 }
 
 int main() {
@@ -15,8 +23,9 @@ int main() {
     cout << "============================================================" << endl;
     cout << "\nHow many elements would you like in the array? ";
     cin >> N;
-
     cout << "\n============================================================" << endl;
+    loading();
+    cout << "The Value of N is: " << N << endl;
 
     return 0;
 }
