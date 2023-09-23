4c8cbf1b7636a0cdc3d095a3ac87848fe25bffcb
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-09-23 22:47:14 +0800

    Update

--- cpp-practice/sortArr_Sudaria.cpp
+++ cpp-practice/sortArr_Sudaria.cpp
@@ -2,6 +2,30 @@
 #include <windows.h>
 using namespace std;
 
+void Sort_Arr(string Arr1[], int N) {
+    for (int i = 0; i < N; i++) {
+        cout << Arr1[i][0] << endl;
+    }
+
+    int Ctr1 = 0;
+    while (Ctr1 <= N - 2) {
+        int Ctr2 = Ctr1 + 1;
+        while (Ctr2 <= N) {
+            if (Arr1[Ctr1][0] > Arr1[Ctr2][0]) {
+                string temp = Arr1[Ctr1];
+                Arr1[Ctr1] = Arr1[Ctr2];
+                Arr1[Ctr2] = temp;
+            }
+            Ctr2++;
+        }
+        Ctr1++;
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << Arr1[i] << " ";
+    }
+}
+
 void loading() {
     for (int i = 0; i < 5; i++) {
         Sleep(90);
@@ -13,19 +37,21 @@ void loading() {
         Sleep(90);
         cout << "\b-";
     }
-    cout << "\b";
+    cout << "\b" << endl;
 }
 
 int main() {
 
     int N;
 
+    string arr1[] = {"Belle", "Lil'o", "Rae", "Ash", "Cyril"};
+
     cout << "============================================================" << endl;
     cout << "\nHow many elements would you like in the array? ";
     cin >> N;
     cout << "\n============================================================" << endl;
     loading();
-    cout << "The Value of N is: " << N << endl;
+    Sort_Arr(arr1, N);
 
     return 0;
 }
