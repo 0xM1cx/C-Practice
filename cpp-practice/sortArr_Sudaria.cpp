a58c55123e60b979e32dd38e721d1eb5debdeba9
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-09-24 01:27:42 +0800

    Update

--- cpp-practice/sortArr_Sudaria.cpp
+++ cpp-practice/sortArr_Sudaria.cpp
@@ -3,16 +3,14 @@
 using namespace std;
 
 void Sort_Arr(string Arr1[], int N) {
-    for (int i = 0; i < N; i++) {
-        cout << Arr1[i][0] << endl;
-    }
+    int Ctr1 = 0, Ctr2;
+    string temp;
 
-    int Ctr1 = 0;
     while (Ctr1 <= N - 2) {
-        int Ctr2 = Ctr1 + 1;
-        while (Ctr2 <= N) {
+        Ctr2 = Ctr1 + 1;
+        while (Ctr2 <= N - 1) {
             if (Arr1[Ctr1][0] > Arr1[Ctr2][0]) {
-                string temp = Arr1[Ctr1];
+                temp = Arr1[Ctr1];
                 Arr1[Ctr1] = Arr1[Ctr2];
                 Arr1[Ctr2] = temp;
             }
@@ -40,18 +38,30 @@ void loading() {
     cout << "\b" << endl;
 }
 
+void push(string Arr1[], int N) {
+    int arr_len = sizeof(Arr1) / sizeof(Arr1[0]);
+    cout << arr_len;
+}
+
 int main() {
 
     int N;
-
-    string arr1[] = {"Belle", "Lil'o", "Rae", "Ash", "Cyril"};
-
+    string arr1[0];
     cout << "============================================================" << endl;
     cout << "\nHow many elements would you like in the array? ";
     cin >> N;
+
+    if (N < 3) {
+        cout << "Elements in the arry must be greater than 3!";
+        loading();
+        main();
+    }
+
     cout << "\n============================================================" << endl;
     loading();
-    Sort_Arr(arr1, N);
+
+    push(arr1, N);
+    // Sort_Arr(arr1, N);
 
     return 0;
 }
