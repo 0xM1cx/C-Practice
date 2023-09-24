7c49884085e034133d4b6648f42284e9b7ccf0ba
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-09-24 17:59:17 +0800

    updated

--- cpp-practice/mergeSort.cpp
+++ cpp-practice/mergeSort.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
+
+void arr1(vector<string> Arr3, int Count3, vector<string> Arr1, int Count1) {
+    Arr3[Count3] = Arr1[Count1];
+    Count1++;
+}
+
+void arr2(vector<string> Arr3, int Count3, vector<string> Arr2, int Count2) {
+    Arr3[Count3] = Arr2[Count2];
+    Count2++;
+}
+
+vector<string> Merge_Arr(vector<string> Arr1, vector<string> Arr2, int N1, int N2) {
+    int Count1 = 1;
+    int Count2 = 1;
+    int Count3 = 1;
+
+    vector<string> Arr3;
+
+    while ((Count1 <= N1) || (Count2 <= N2)) {
+        if (Count1 > N1) {
+            arr2(Arr3, Count3, Arr2, Count2);
+        } else {
+            if (Count2 > N2) {
+                arr1(Arr3, Count3, Arr1, Count1);
+            } else {
+                if (Arr1[Count1] < Arr2[Count2]) {
+                    arr1(Arr3, Count3, Arr1, Count1);
+                } else {
+                    arr2(Arr3, Count3, Arr2, Count2);
+                }
+            }
+        }
+    }
+}
 
 int main() {
 
-    string arr1[] = {"Mango", "Orange", "Apple"};
-    string arr2[] = {"Banana", "Grape"};
+    vector<string> Arr1 = {"Mango", "Orange", "Apple"};
+    vector<string> Arr2 = {"Banana", "Grape"};
+
+    int N1 = Arr1.size(), N2 = Arr2.size();
+    Merge_Arr(Arr1, Arr2, N1, N2);
 
     return 0;
 }
