b07ed1a508f20b3ce9e3fe24ea1263ab5694b586
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-09-27 13:26:04 +0800

    update

---
+++ DSA/representingArrayinMem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *start = &arr[0];
+    // int s = (int)start;
+    int k = sizeof(arr[1]);
+    int i = 2;
+    // printf("%d", *start);
+    int loc = *start + k * (i - 1);
+    printf("%x", loc);
+    return 0;
+}
