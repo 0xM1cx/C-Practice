cb56e034a4672fcb1de2ed97faab5e1e54349487
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-09-28 06:24:25 +0800

    Updated

--- DSA/LinearDSA/Sort_ArrAss.c
+++ DSA/LinearDSA/Sort_ArrAss.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char arr1[10][100] = {"Belle", "Rae", "Ash", "Lil'o", "Ash"};
-    char myname[100] = {"Belle",
-                        "Rae",
-                        "Ash",
-                        "Lil'o",
-                        "Ash"};
+    char arr1[][100] = {"Belle", "Rae", "Ash", "Lil'o", "Ash"};
+    char *names[] = {"Belle", "Rae", "Ash", "Lil'o", "Ash"};
+    char *myname = "Shawn";
     int len = sizeof(arr1) / sizeof(arr1[0]);
+
     printf("%d", len);
     printf("\n%s", myname);
     return 0;
