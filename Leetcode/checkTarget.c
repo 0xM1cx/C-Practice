def2c5f03a4e3810b10c0cd07b52f81cc74d3ec2
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-20 20:07:59 +0800

    update

--- Leetcode/checkTarget.c
+++ Leetcode/checkTarget.c
@@ -3,24 +3,29 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
-    int *arr = (int *)malloc(returnSize * sizeof(int));
 
+void twoSum(int *nums, int numsSize, int target) {
+    int *arr = (int *)malloc(2 * sizeof(int));
+    int flag = 0;
     for (int i = 0; i < numsSize; i++) {
+        if (flag == 1) {
+            break;
+        }
+
         for (int j = i + 1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
                 arr[0] = i;
                 arr[1] = j;
-                return arr;
+                break;
+                flag = 1;
             }
         }
     }
+
+    printf("[%d, %d]", arr[0], arr[1]);
 }
 
 int main() {
-    int *arr = {2, 7, 11, 15};
-    int *re = {1, 1};
-    twoSum(arr, 4, 9, re);
-
-    free(arr);
+    int arr[] = {3, 3};
+    twoSum(arr, 4, 6);
 }
---
+++ Leetcode/sparseMatrix.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int sparseMatrix[][5] = {
+        {0, 0, 0, 3, 0},
+        {16, 0, 0, 47, 11},
+        {0, 0, 88, 0, 23},
+        {0, 17, 0, 0, 0},
+        {0, 0, 12, 0, 0}};
+
+    int x, y;
+
+    int altern[9][3];
+
+    int rowCount = 0;
+
+    for (int i = 0; i < 5; i++) {
+
+        for (int j = 0; j < 5; j++) {
+            if (sparseMatrix[i][j] != 0) {
+                altern[rowCount][0] = i;
+                altern[rowCount][1] = j;
+                altern[rowCount][2] = sparseMatrix[i][j];
+            }
+        }
+        rowCount++;
+    }
+
+    printf("| %d |", altern[0][0]);
+    printf(" %d |", altern[0][1]);
+    printf(" %d |", altern[0][2]);
+}
