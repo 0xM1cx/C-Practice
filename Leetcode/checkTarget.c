a01dd64f080609412541d4ede7454efc6ee7db7e
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-20 15:21:58 +0800

    update

---
+++ Leetcode/checkTarget.c
@@ -0,0 +1,24 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
+    int *arr = (int *)malloc(returnSize * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                arr[0] = i;
+                arr[1] = j;
+                return arr;
+                free(arr);
+            }
+        }
+    }
+}
+
+int main() {
+    int *arr = {2, 7, 11, 15};
+    twoSum(arr, 4, 8, 2);
+}
