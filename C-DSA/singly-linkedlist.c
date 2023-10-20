5667e8cc96f9416380f9f10e08af24c1acd3373d
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-20 23:59:59 +0800

    updated

--- C-DSA/singly-linkedlist.c
+++ C-DSA/singly-linkedlist.c
@@ -7,12 +7,54 @@ struct node {
     struct node *link;
 };
 
+struct node *createNode(char data[]) {
+    struct node *newNode = malloc(sizeof(struct node));
+
+    strcpy(newNode->data, data);
+    newNode->link = NULL;
+
+    return newNode;
+}
+
+struct node *insertNewNodeAtBeginning(struct node *head, char data[]) {
+    struct node *newNode = createNode(data);
+    newNode->link = head;
+    return newNode;
+}
+
 void main() {
-    struct node *head = malloc(sizeof(struct node));
+    struct node *head = NULL;
 
-    strcpy(head->data, "SH");
-    head->link = NULL;
+    head = insertNewNodeAtBeginning(head, "SH");
+    head = insertNewNodeAtBeginning(head, "ZJ");
+    head = insertNewNodeAtBeginning(head, "SM");
+    head = insertNewNodeAtBeginning(head, "ZD");
 
-    printf("%s\n", head->data);
-    printf("%x", head->link);
+    struct node *current = head;
+    while (current != NULL) {
+        printf("%s->", current->data);
+        current = current->link;
+    }
+    printf("NULL");
+
+    // strcpy(head->data, "SH");
+    // head->link = NULL;
+
+    // struct node *second = malloc(sizeof(struct node));
+    // strcpy(second->data, "ZJ");
+    // second->link = NULL;
+
+    // struct node *third = malloc(sizeof(struct node));
+    // strcpy(third->data, "SM");
+    // third->link = NULL;
+
+    // head->link = second;
+    // second->link = third;
+
+    // printf("%s\n", head->data);
+    // printf("%x\n", head->link);
+    // printf("%s\n", head->link->data);
+    // printf("%x\n", second->link);
+    // printf("%s\n", second->link->data);
+    // printf("%x", second->link->link);
 }
