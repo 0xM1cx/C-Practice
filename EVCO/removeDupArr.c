dea0b310463ad06d6833641e3d6612e46aa8dd15
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-21 20:36:42 +0800

    Update

--- EVCO/removeDupArr.c
+++ EVCO/removeDupArr.c
@@ -6,6 +6,7 @@ void main() {
     scanf("%d", &size);
 
     int arr[size];
+    int nodup[50];
     printf("Enter elements in array: \n");
     for (int i = 0; i < size; i++) {
         int element;
@@ -13,21 +14,35 @@ void main() {
         arr[i] = element;
     }
 
-    for (int j = 0; j < size; j++) {
-        for (int n = j + 1; n < size;) {
-            if (arr[j] == arr[n]) {
-                for (int m = j; m < size - 1; m++) {
-                    arr[m] = arr[m + 1];
-                }
-                size--;
-            } else {
-                n++;
+    // for (int j = 0; j < size; j++) {
+    //     for (int n = j + 1; n < size;) {
+    //         if (arr[j] == arr[n]) {
+    //             for (int m = j; m < size; m++) {
+    //                 arr[m] = arr[m + 1];
+    //             }
+    //             size--;
+    //         } else {
+    //             n++;
+    //         }
+    //     }
+    // }
+    int count = 0;
+    for (int h = 0; h < size; h++) {
+        int flag = 0;
+        for (int u = h + 1; u < size; u++) {
+            if (arr[h] == arr[u]) {
+                flag = 1;
+                break;
             }
+        }
+        if (flag == 0) {
+            nodup[count] = arr[h];
+            count++;
         }
     }
 
     printf("Array after removing duplicates: ");
-    for (int h = 0; h < size; h++) {
-        printf("%d ", arr[h]);
+    for (int h = 0; h < count; h++) {
+        printf("%d ", nodup[h]);
     }
 }
