407d8a7aede42cf6d7ca0f821791e9b6ec63a968
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-22 23:55:02 +0800

    update

--- EVCO/ivanChal.c
+++ EVCO/ivanChal.c
@@ -4,20 +4,15 @@ void main() {
     char name[100];
     printf("INPUT: ");
     fgets(name, sizeof(name), stdin);
-    int count = 0;
+    int ctr = 0;
+    int flag = 0;
     for (int i = 0; i < strlen(name); i++) {
-        int flag = 0;
-        for (int k = 0; k <= strlen(name); k++) {
-            for (int j = count; j < k; j++) {
-                printf("%c", name[j]);
+        for (int j = i; j < strlen(name); j++) {
+            for (int k = ctr; k < j; k++) {
+                printf("%c ", name[k]);
             }
-            if (flag < strlen(name)) {
-                printf("\n");
-                flag++;
-            } else {
-                break;
-            }
+            printf("\n");
         }
-        count++;
+        ctr++;
     }
 }
