314c51f1caa09a969bcabf1ef39071b13e899e46
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-29 08:20:49 +0800

    Update

---
+++ cpp-practice/DSA/stack.cpp
@@ -0,0 +1,58 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void loadingAnimation() {
+    for (int i = 1; i <= 3; i++) {
+        printf("\b-");
+        this_thread::sleep_for(chrono::milliseconds(100));
+        printf("\b\\");
+        this_thread::sleep_for(chrono::milliseconds(100));
+        printf("\b|");
+        this_thread::sleep_for(chrono::milliseconds(100));
+        printf("\b/");
+        this_thread::sleep_for(chrono::milliseconds(100));
+        printf("\b");
+    }
+}
+
+int AskValue() {
+}
+
+int *Push(int value) {
+}
+
+int *Pop() {
+}
+
+int main() {
+
+    int userOption;
+    printf("[1] Push()\n[2] Pop()\n[3] Exit\n");
+    printf("OPTION: ");
+    scanf("%d", &userOption);
+
+    if (userOption < 1 || userOption > 3) {
+        printf("Number must be within the options!!\n");
+        loadingAnimation();
+        main();
+    }
+
+    switch (userOption) {
+    case 1:
+        int value = AskValue();
+        Push(value);
+        break;
+    case 2:
+        Pop();
+        break;
+    case 3:
+        printf("Goodbye!!...");
+        loadingAnimation();
+        exit(0);
+        break;
+    default:
+        break;
+    }
+}
