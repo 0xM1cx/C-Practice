7c598efcfeb80226ee1d7fbe3a0e51f2ac0785dd
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-10-31 16:52:23 +0800

    Update

--- cpp-practice/DSA/stack.cpp
+++ cpp-practice/DSA/stack.cpp
@@ -1,8 +1,25 @@
 #include <chrono>
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <map>
+#include <string.h>
 #include <thread>
 #include <vector>
 using namespace std;
+
+vector<string> Arr;
+int Top = 0, counter = 1;
+map<int, vector<string>> dictionary;
+
+void loadingAnimation(), AddToMap(string Method_Used, string retVal, string stack_content), Push(string value), Pop(), MainMenu();
+string AskValue();
+int DisplayTable();
+
+int main() {
+
+    MainMenu();
+}
 
 void loadingAnimation() {
     for (int i = 1; i <= 3; i++) {
@@ -18,45 +35,25 @@ void loadingAnimation() {
     }
 }
 
-void DisplayTable() {
-}
-
-int AskValue() {
-    int num;
-    printf("Number to Push: ");
-    scanf("%d", &num);
-    return num;
-}
-
-int Push(int value, vector<int> Arr, int Top) {
-    Arr[Top] = value;
-    return Top++;
-}
-
-void Pop(vector<int> Arr, int Top) {
-    printf("%d", Arr[Top]);
-    Top--;
-}
-
-int main() {
-
-    int userOption, Top = 0, value;
+void MainMenu() {
+    int userOption;
+    string value;
     printf("[1] Push()\n[2] Pop()\n[3] Exit\n");
     printf("OPTION: ");
     scanf("%d", &userOption);
-    vector<int> Arr;
     if (userOption < 1 || userOption > 3) {
         printf("Number must be within the options!!\n");
         loadingAnimation();
-        main();
+        MainMenu();
     }
+
     switch (userOption) {
     case 1:
         value = AskValue();
-        Top = Push(value, Arr, Top);
+        Push(value);
         break;
     case 2:
-        Pop(Arr, Top);
+        Pop();
         break;
     case 3:
         printf("Goodbye!!...");
@@ -65,5 +62,60 @@ int main() {
         break;
     default:
         break;
+    }
+}
+
+void AddToMap(string Method_Used, string retVal, string stack_content) {
+    dictionary[counter] = {
+        Method_Used,
+        retVal,
+        stack_content};
+    counter++;
+}
+
+int DisplayTable() {
+    system("cls");
+    cout << setw(15) << left << "Method Used"
+         << setw(1) << left << " "
+         << setw(15) << left << "Return Value"
+         << setw(1) << left << " "
+         << setw(25) << left << "Stack Content" << endl;
+
+    for (int i = 1; i <= counter; i++) {
+        cout << setw(15) << left << dictionary[i][0]
+             << setw(1) << left << " "
+             << setw(15) << left << dictionary[i][1]
+             << setw(1) << left << " "
+             << setw(25) << left << dictionary[i][2] << endl;
+    }
+}
+
+string AskValue() {
+    string input;
+    printf("Number to Push: ");
+    cin >> input;
+    return input;
+}
+
+void Push(string value) {
+    Arr.push_back(value);
+    Top++;
+    string combined;               // To hold all the array elements in a single string separeted by spaces
+    for (const string &el : Arr) { // Para mag iterate over every element ha Arr na array
+        combined += el + " ";
+    }
+
+    AddToMap("Push()", "-", combined);
+    DisplayTable();
+    cout << "Bye";
+}
+
+void Pop() {
+    if (Top != 0) {
+
+        DisplayTable();
+        Top--;
+    } else {
+        printf("Stack is Empty");
     }
 }
