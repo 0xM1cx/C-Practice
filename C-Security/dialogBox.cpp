8d19f1616d24c8d2050a0e063702cdbbe9cd2a14
Shawn Sudaria <shawnlovecode14@gmail.com>
2024-02-09 01:52:32 +0800

    update

--- C-Security/dialogBox.cpp
+++ C-Security/dialogBox.cpp
@@ -3,16 +3,21 @@
 #include <winuser.h>
 
 using namespace std;
+
 int main() {
     int msgBox = MessageBox(
         NULL,
         "You have been pwned",
-        "THIS IS A SAFE PROGRAM",
+        "THIS IS A UNSAFE PROGRAM",
         MB_ICONWARNING | MB_YESNO);
 
     if (msgBox == IDYES) {
         for (int i = 0; i < 10; i++) {
             MessageBox(NULL, "HALA KA MAY VIRUS IM LAPTOP", "May klase buwas?", MB_ICONWARNING);
+        }
+    }else if (msgBox == IDNO) {
+        for(int i = 0; i < 30; i++){
+          MessageBox(NULL, "NA VIRUSAN NA IM LAPPY", "HEHEHEHEH", MB_ICONWARNING);
         }
     }
     return 0;
