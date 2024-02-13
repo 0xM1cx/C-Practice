3b3d2b4cbbdc3eeabd4d07a16354192be438e26b
Shawn Sudaria <shawnlovecode14@gmail.com>
2024-02-13 17:39:22 +0800

    Update

--- C-Security/bufferOverflow.c
+++ C-Security/bufferOverflow.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char ans[4];
-    int test;
+    int test = 0;
     printf("INPUT: ");
-    scanf("%s", ans);
-    if (test == 1) {
-        printf("Flag{Gwapo}");
+    gets(ans);
+    if (!(strcmp(ans, "pass"))) {
+        printf("Correct Password!!");
     } else {
-        printf("INCORRECT: %s", ans);
+        printf("HEHEHEHEHEH");
+    }
+
+    if (test) {
+        printf("Welcome Root user");
+        printf("%d", test);
     }
     return 0;
 }
