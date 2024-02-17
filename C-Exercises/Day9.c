8b68fc101d63a824348184c9f5533eb9b43e20e3
Shawn Sudaria <shawnlovecode14@gmail.com>
2024-02-17 13:05:51 +0800

    Update

---
+++ C-Exercises/Day9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int isPrime(int n) {
+    if (n == 1) {
+        return 0;
+    } else if (n <= 3) {
+        return 1;
+    } else {
+        if (n % 2 != 0 || n % 3 != 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+int main() {
+    int bool = isPrime(4);
+    if (bool == 1) {
+        printf("Is Prime Number");
+    } else if (bool == 0) {
+        printf("Is Composite");
+    }
+
+    return 0;
+}
